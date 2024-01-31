51bca23fd8418a294ddd2a7d249daa34f6aff61e
Zohaib Rauf <zabirauf@gmail.com>
2024-01-30 23:39:35 -0800

    Added recording of audio

--- OS/01/app/audio-server/src/main.rs
+++ OS/01/app/audio-server/src/main.rs
@@ -1,8 +1,10 @@
 mod transcribe;
+mod record;
 
 use clap::Parser;
 use std::path::PathBuf;
 use transcribe::transcribe;
+use record::record_wav_audio;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -12,8 +14,8 @@ struct Args {
     model_path: PathBuf,
     
     /// This is the wav audio file that will be converted from speech to text
-    #[arg(short, long, value_parser, required = true)]
-    file_path: PathBuf,
+    #[arg(short, long, value_parser)]
+    file_path: Option<PathBuf>,
 }
 
 fn main() {
@@ -21,9 +23,13 @@ fn main() {
     let args = Args::parse();
 
     println!("Model: {}", args.model_path.display());
-    println!("File: {}", args.file_path.display());
 
-    let result = transcribe(&args.model_path, &args.file_path);
+    let file_path = match args.file_path {
+        Some(fp) => fp,
+        None => PathBuf::from(record_wav_audio(3).expect("Error recording"))
+    };
+
+    let result = transcribe(&args.model_path, &file_path);
 
     match result {
         Ok(transcription) => println!("Transcription:\n{}", transcription),
